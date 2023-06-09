02da2134b3cbc475813793ff4e75af2007686c6a
DevaWinner <winneraaniekan@gmail.com>
2023-06-09 22:41:24 +0100

    Add function to save task to local storage

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
 import _ from 'lodash';
 import './style.css';
 
-const tasks = [
-  { description: 'wash the dishes', completed: false, index: 1 },
-  { description: 'complete to do list', completed: true, index: 2 },
-  { description: 'learn Javascript', completed: false, index: 3 },
-];
+const tasksKey = 'todoTasks';
 
-const renderTasks = () => {
-  const todoList = document.getElementById('todo-list');
+let tasks = [];
 
-  tasks.sort((a, b) => a.index - b.index);
-
-  tasks.forEach(task => {
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = task.completed;
-    checkbox.addEventListener('change', () => {
-      task.completed = checkbox.checked;
-    });
-
-    const listItem = document.createElement('li');
-    listItem.innerText = task.description;
-    if (task.completed) {
-      listItem.classList.add('completed');
-    }
-    
-    listItem.appendChild(checkbox);
-    todoList.appendChild(listItem);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', renderTasks);
+const saveTasks = () => {
+  localStorage.setItem(tasksKey, JSON.stringify(tasks));
+};
